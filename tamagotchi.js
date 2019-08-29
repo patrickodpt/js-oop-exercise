7d1afa591c2f0359114c8443613e3af07a3c1273
Patrick O'Shea <patrickodpt@gmail.com>
2019-08-29 18:41:28 -0400

    part 3: added new methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -14,12 +14,25 @@ class Tamagotchi {
     console.log(`${this.name} has ${this.foodInTummy} food left in tummy.`);
     console.log(`${this.name} is unhappy! WAH!!!`)
   }
+  puke() {
+    this.foodInTummy -= 1
+    console.log(`${this.name} puked up some food. It has ${this.foodInTummy}
+      left in tummy. WAHHHHH!`);
+  }
+  yawn() {
+    this.restedness -= 1
+    console.log(`${this.name} has current restedness of: ${this.restedness}`)
+  }
 }
 
 //create new Tamagotchis
 let tama0 = new Tamagotchi('Jim', 'salesman')
 let tama1 = new Tamagotchi('Pam', 'artist')
+let tama2 = new Tamagotchi('Dwight', 'farmer')
+let tama3 = new Tamagotchi('M. Scott', 'manager')
 
 //test out your Tamagotchies below via console.logs
 tama0.cry()
 tama1.cry()
+tama2.puke()
+tama3.yawn()
